2d712bd81f7dba19da8fc0afc3c50fa05f931c12
Rodrigo Perez Domanico <rodrigoperez33@hotmail.com>
2023-05-05 11:10:20 -0300

    Initial commit.

---
+++ src/firstone_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { firstone_backend } from "../../declarations/firstone_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await firstone_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
